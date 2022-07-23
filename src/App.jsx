6da256d806a40f697ef5e60cc5bb4738f294e964
Roche Harold <rocheharold13@gmail.com>
2022-07-23 21:55:51 +0300

    update changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Top from "./components/top/Top";
+import Home from "./components/home/Home";
+import Property from "./components/property/Property";
+import Listing from "./components/listing/Listing";
+import Contacts from "./components/listing/Listing";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Top />
+      <Home />
+      <Property />
+      <Listing />
+      <Contacts />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
