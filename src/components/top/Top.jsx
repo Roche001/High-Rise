b30c22cb604e4af47bf6afb69c2147d49651d5c9
Roche Harold <rocheharold13@gmail.com>
2022-07-24 22:21:08 +0300

    update changes

--- src/components/top/Top.jsx
+++ src/components/top/Top.jsx
@@ -12,30 +12,32 @@ const Top = () => {
       <Container>
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link>
-              <Link to="/" className="link">
-                <li>Home</li>
-              </Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/Property" className="link">
-                <li>Property</li>
-              </Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/Listing" className="link">
-                <li>Listing</li>
-              </Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/Contacts" className="link">
-                <li>Contacts</li>
-              </Link>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        <div className="nav-items">
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link>
+                <Link to="/" className="link">
+                  <li>Home</li>
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/Property" className="link">
+                  <li>Property</li>
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/Listing" className="link">
+                  <li>Listing</li>
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/Contacts" className="link">
+                  <li>Contacts</li>
+                </Link>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </div>
       </Container>
     </Navbar>
   );
