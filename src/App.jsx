f5826587b3f1ff2050d5b9a5f2b73da13cf02506
Roche Harold <rocheharold13@gmail.com>
2022-07-24 22:09:09 +0300

    update changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Top from "./components/top/Top";
 import Home from "./components/home/Home";
 import Property from "./components/property/Property";
