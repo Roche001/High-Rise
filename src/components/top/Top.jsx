44d3dea0f130eb13214608289f75eb10a7d1fc2a
Roche Harold <rocheharold13@gmail.com>
2022-07-25 20:16:51 +0300

    update changes

--- src/components/top/Top.jsx
+++ src/components/top/Top.jsx
@@ -8,36 +8,41 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 
 const Top = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      bg="transparent"
+      variant="dark"
+      className="navbar"
+    >
       <Container>
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <div className="nav-items">
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link>
-                <Link to="/" className="link">
-                  <li>Home</li>
-                </Link>
-              </Nav.Link>
-              <Nav.Link>
-                <Link to="/Property" className="link">
-                  <li>Property</li>
-                </Link>
-              </Nav.Link>
-              <Nav.Link>
-                <Link to="/Listing" className="link">
-                  <li>Listing</li>
-                </Link>
-              </Nav.Link>
-              <Nav.Link>
-                <Link to="/Contacts" className="link">
-                  <li>Contacts</li>
-                </Link>
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </div>
+
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link>
+              <Link to="/" className="link">
+                <li>Home</li>
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/Property" className="link">
+                <li>Property</li>
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/Listing" className="link">
+                <li>Listing</li>
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/Contacts" className="link">
+                <li>Contacts</li>
+              </Link>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
