b6aa1b4af19e35dd32df2661939daa5c6cc94503
Roche Harold <rocheharold13@gmail.com>
2022-07-27 18:22:09 +0300

    update changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Footer.css";
 const Footer = () => {
-  return <div>Footer</div>;
+  return <div className="container-fluid"></div>;
 };
 
 export default Footer;
