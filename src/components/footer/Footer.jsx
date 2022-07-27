a5a742f43eb7fac72e2933df11e1c71136ab88fd
Roche Harold <rocheharold13@gmail.com>
2022-07-27 18:44:42 +0300

    update changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import "./Footer.css";
+import { RiHome6Line } from "react-icons/ri";
 const Footer = () => {
-  return <div className="container-fluid"></div>;
+  return (
+    <div className="footer container-fluid">
+      <div className="rise">
+        <div className="rise-logo">
+          <RiHome6Line />
+        </div>
+        <div className="rise-text">
+          <h3>High-Rise </h3>
+          <p>REAL ESTATE COMPANY</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
