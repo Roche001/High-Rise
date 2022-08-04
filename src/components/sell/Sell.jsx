4bc2b228f3e1ff184e3a15ab245e160dc4182a1d
Roche Harold <rocheharold13@gmail.com>
2022-08-04 16:59:45 +0300

    update changes

--- src/components/sell/Sell.jsx
+++ src/components/sell/Sell.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import "./Offices.css";
-const Offices = () => {
+import "./Sell.css";
+const Sell = () => {
   return <div></div>;
 };
 
-export default Offices;
+export default Sell;
