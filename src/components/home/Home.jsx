4c722d39dd667c8581316aef493e255a4ae9dbd8
Roche Harold <rocheharold13@gmail.com>
2022-08-05 12:54:00 +0300

    update changes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,6 +3,41 @@ import "./Home.css";
 const Home = () => {
   return (
     <section className="home container-fluid">
+      <div className="photo">
+        <div className="search">
+          <select className="picker">
+            <option value="Buy">Buy</option>
+            <option value="Rent">Rent</option>
+            <option value="Sold">Sold</option>
+          </select>
+          <select className="picker">
+            <option value="Apartment">Apartment</option>
+            <option value="Offices">Offices</option>
+            <option value="Townhome">Townhome</option>
+            <option value="Houses">Houses</option>
+            <option value="Studio">Studio</option>
+          </select>
+
+          <input
+            className="required"
+            type="search"
+            name="seach"
+            placeholder="Search"
+            required
+          />
+
+          <button className="search-button">
+            <a href="#search">
+              <h6>
+                <span>
+                  <GoSearch />
+                </span>
+                Search
+              </h6>
+            </a>
+          </button>
+        </div>
+      </div>
       <div className="home-header row text-center">
         <h3>Discover Our Featured Listings</h3>
         <p>
