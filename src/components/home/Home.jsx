1af7b7f747a10e6e9f3df9bbe594329e8206a4ce
Roche Harold <rocheharold13@gmail.com>
2022-08-05 15:02:36 +0300

    update changes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import "./Home.css";
 const Home = () => {
-  return (
-    <section className="home container-fluid">
-      <h1>Hello</h1>
-    </section>
-  );
+  return;
 };
 
 export default Home;
