f6495418ccb77fa11bfc07f9818d3e0bc9fa2dee
Roche Harold <rocheharold13@gmail.com>
2022-08-05 16:22:59 +0300

    update changes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,7 +3,14 @@ import "./Home.css";
 const Home = () => {
   return (
     <section className="home container-fluid">
-      <div className="home-header"></div>
+      <div className="home-header">
+        .<h2>Your Property is Our Priority</h2>
+        <p>
+          A small river named Duden flows by their place and supplies it with
+          the necessary regelialia. It is a paradisematic country, in which
+          roasted parts of sentences fly into your mouth
+        </p>
+      </div>
     </section>
   );
 };
