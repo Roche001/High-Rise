eaf5efabc8bcb134b47ff8c73aabff08bbb2bcd1
Roche Harold <rocheharold13@gmail.com>
2022-08-14 12:56:43 +0300

    update changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Rent from "./components/rent/Rent";
 import Buy from "./components/buy/Buy";
 import Sell from "./components/sell/Sell";
 import Footer from "./components/footer/Footer";
+import House1 from "./components/house1/House1";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Route Rent path="Rent" exact element={<Rent />} />
         <Route Buy path="Buy" exact element={<Buy />} />
         <Route Sell path="Sell" exact element={<Sell />} />
+        <Route House1 path="House1" exact element={<House1 />} />
       </Routes>
       <Footer />
     </Router>
---
+++ src/components/house1/House1.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./House1.css";
+
+const House1 = () => {
+  return <div>House1</div>;
+};
+
+export default House1;
