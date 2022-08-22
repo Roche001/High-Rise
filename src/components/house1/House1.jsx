2c0217da27c6aabc027d17f37a820029b74ac1c8
Roche Harold <rocheharold13@gmail.com>
2022-08-22 18:34:04 +0300

    update changes

--- src/components/house1/House1.jsx
+++ src/components/house1/House1.jsx
@@ -299,43 +299,43 @@ const House1 = () => {
               <Accordion.Item eventKey="0">
                 <Accordion.Header>
                   <div className="plan">
+                    <h5>Ground Floor Plan</h5>
+                  </div>
+                </Accordion.Header>
+                <Accordion.Body>
+                  <img
+                    className="d-block w-100"
+                    src="./assets/ground1.jpeg"
+                    alt="Ground Floor"
+                  />
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="1">
+                <Accordion.Header>
+                  <div className="plan">
                     <h5>First Floor Plan</h5>
                   </div>
                 </Accordion.Header>
-                <Accordion.Body></Accordion.Body>
+                <Accordion.Body>
+                  <img
+                    className="d-block w-100"
+                    src="./assets/ground2.jpeg"
+                    alt="Ground Floor"
+                  />
+                </Accordion.Body>
               </Accordion.Item>
-              <Accordion.Item eventKey="1">
+              <Accordion.Item eventKey="3">
                 <Accordion.Header>
                   <div className="plan">
                     <h5>Second Floor Plan</h5>
                   </div>
                 </Accordion.Header>
                 <Accordion.Body>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
-                </Accordion.Body>
-              </Accordion.Item>
-              <Accordion.Item eventKey="3">
-                <Accordion.Header>
-                  <div className="plan">
-                    <h5>Third Floor Plan</h5>
-                  </div>
-                </Accordion.Header>
-                <Accordion.Body>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
+                  <img
+                    className="d-block w-100"
+                    src="./assets/ground3.gif"
+                    alt="Ground Floor"
+                  />
                 </Accordion.Body>
               </Accordion.Item>
             </Accordion>
