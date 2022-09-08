f19e922b3eefd7dfebe773d735100c86e228cce4
Roche Harold <rocheharold13@gmail.com>
2022-09-08 08:52:22 +0300

    update changes

--- src/components/house9/House9.jsx
+++ src/components/house9/House9.jsx
@@ -54,43 +54,55 @@ const House9 = () => {
           <img src="./assets/count14.jpg" alt="Featured-House" />
         </Carousel.Item>
       </Carousel>
-      <div className="overview row">
-        <div className="overview-one ">
-          <h4>
-            <small> Condo</small> <span>property Type</span>
-          </h4>
 
-          <h4>
-            <small>
-              <IoIosBed />2
-            </small>
-            <span>Bedrooms</span>
-          </h4>
-          <h4>
-            <small>
-              <GiShower />3
-            </small>
-            <span>Bathrooms</span>
-          </h4>
-          <h4>
-            <small>
-              <GiHomeGarage />3
-            </small>
-            <span>Garages</span>
-          </h4>
-          <h4>
-            <small>
-              <AiOutlineApartment />2
-            </small>
-            <span>Sq ft</span>
-          </h4>
-          <h4>
-            <small>
-              <FcCalendar /> 2009
-            </small>
-            <span>Year Built</span>
-          </h4>
-        </div>
+      <div className="pallets-one ">
+        <h4>
+          <small> Condo</small> <span>property Type</span>
+        </h4>
+
+        <h4>
+          <small>
+            <IoIosBed />2
+          </small>
+          <span>Bedrooms</span>
+        </h4>
+        <h4>
+          <small>
+            <GiShower />3
+          </small>
+          <span>Bathroom</span>
+        </h4>
+        <h4>
+          <small>
+            <GiHomeGarage />3
+          </small>
+          <span>Garages</span>
+        </h4>
+        <h4>
+          <small>
+            <AiOutlineApartment />2
+          </small>
+          <span>Sq ft</span>
+        </h4>
+        <h4>
+          <small>
+            <FcCalendar /> 2009
+          </small>
+          <span>Year Built</span>
+        </h4>
+      </div>
+      <div className="description">
+        <h3>Description</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+          nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+          volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+          ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+          Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse
+          molestie consequat, vel illum dolore eu feugiat nulla facilisis at
+          vero eros et accumsan et iusto odio dignissim qui blandit praesent
+          luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+        </p>
       </div>
     </div>
   );
