78e3f067f3617e6682b221c58cf28ddd4fff9630
Roche Harold <rocheharold13@gmail.com>
2022-09-26 16:07:00 +0300

    update changes

--- src/components/rent/Rent.jsx
+++ src/components/rent/Rent.jsx
@@ -309,19 +309,7 @@ const Rent = () => {
         <div className="ranches">
           <h3>Raches and Farms </h3>
         </div>
-        <div className="ranches-container">
-          <Card style={{ width: "20rem" }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </div>
+        <div className="ranches-container"></div>
       </div>
     </section>
   );
