f73d1a8bba467f655743e4fe3563e49694ba0029
Roche Harold <rocheharold13@gmail.com>
2022-10-03 07:18:17 +0300

    update changes

--- src/components/house17/House17.jsx
+++ src/components/house17/House17.jsx
@@ -1,56 +1,76 @@
 import React from "react";
 import "./House17.css";
 import Carousel from "react-bootstrap/Carousel";
+import { MdLocationOn } from "react-icons/md";
+import { GiResize } from "react-icons/gi";
+import { BsFillPeopleFill } from "react-icons/bs";
 const House17 = () => {
   return (
-    <div className="offices-house">
+    <div className="offices-house container">
       <Carousel>
         <Carousel.Item interval={1000} className="sized">
-          <img src="./assets/horse1.webp" alt="Featured-House" />
+          <img src="./assets/officea1.jpg" alt="Featured-House" />
         </Carousel.Item>
         <Carousel.Item interval={500} className="sized">
-          <img src="./assets/horse2.webp" alt="Featured-House" />
+          <img src="./assets/officea2.jpg" alt="Featured-House" />
         </Carousel.Item>
         <Carousel.Item interval={500} className="sized">
-          <img src="./assets/horse3.webp" alt="Featured-House" />
+          <img src="./assets/officea3.jpg" alt="Featured-House" />
         </Carousel.Item>
         <Carousel.Item interval={500} className="sized">
-          <img src="./assets/horse4.webp" alt="Featured-House" />
+          <img src="./assets/officea4.jpg" alt="Featured-House" />
         </Carousel.Item>
         <Carousel.Item interval={500} className="sized">
-          <img src="./assets/horse5.webp" alt="Featured-House" />
+          <img src="./assets/officea5.jpg" alt="Featured-House" />
         </Carousel.Item>
         <Carousel.Item interval={500} className="sized">
-          <img src="./assets/horse6.webp" alt="Featured-House" />
+          <img src="./assets/officea6.jpg" alt="Featured-House" />
         </Carousel.Item>
         <Carousel.Item interval={500} className="sized">
-          <img src="./assets/horse7.webp" alt="Featured-House" />
+          <img src="./assets/officea7.jpg" alt="Featured-House" />
         </Carousel.Item>
         <Carousel.Item interval={500} className="sized">
-          <img src="./assets/horse8.webp" alt="Featured-House" />
+          <img src="./assets/officea8.jpg" alt="Featured-House" />
         </Carousel.Item>
         <Carousel.Item interval={500} className="sized">
-          <img src="./assets/horse9.webp" alt="Featured-House" />
+          <img src="./assets/officea9.jpg" alt="Featured-House" />
         </Carousel.Item>
         <Carousel.Item interval={500} className="sized">
-          <img src="./assets/horse10.webp" alt="Featured-House" />
+          <img src="./assets/officea10.jpg" alt="Featured-House" />
         </Carousel.Item>
         <Carousel.Item interval={500} className="sized">
-          <img src="./assets/horse11.webp" alt="Featured-House" />
+          <img src="./assets/officea11.jpg" alt="Featured-House" />
         </Carousel.Item>
         <Carousel.Item interval={500} className="sized">
-          <img src="./assets/horse12.webp" alt="Featured-House" />
+          <img src="./assets/officea12.jpg" alt="Featured-House" />
         </Carousel.Item>
         <Carousel.Item interval={500} className="sized">
-          <img src="./assets/horse13.webp" alt="Featured-House" />
+          <img src="./assets/officea13.jpg" alt="Featured-House" />
         </Carousel.Item>
         <Carousel.Item interval={500} className="sized">
-          <img src="./assets/horse14.webp" alt="Featured-House" />
-        </Carousel.Item>
-        <Carousel.Item interval={500} className="sized">
-          <img src="./assets/horse15.webp" alt="Featured-House" />
+          <img src="./assets/officea14.jpg" alt="Featured-House" />
         </Carousel.Item>
       </Carousel>
+      <div className="office-features">
+        <h4>
+          <small>
+            <MdLocationOn />
+          </small>
+          <span>Address: Avinguda Diagonal, B 08018 Barcelona</span>
+        </h4>
+        <h4>
+          <small>
+            <GiResize />
+          </small>
+          <span>Office size: 8.457 Sq Ft</span>
+        </h4>
+        <h4>
+          <small>
+            <BsFillPeopleFill />
+          </small>
+          <span>Meeting size: 2 to 24 persons </span>
+        </h4>
+      </div>
     </div>
   );
 };
