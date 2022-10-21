8afab58c550b8adafb22adae593bbb125232bb1d
Roche Harold <rocheharold13@gmail.com>
2022-10-21 14:53:46 +0300

    update changes

--- src/components/sell/Sell.jsx
+++ src/components/sell/Sell.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import "./Sell.css";
 const Sell = () => {
-  return <div></div>;
+  return (
+    <div className="sell-header">
+      <h3>Why Sell Your House with High-Rise</h3>
+      <div className="sell-container">
+        <div className="sell-card"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Sell;
