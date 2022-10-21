defaf547ca87039563bb8902cab13720fa8b1bb3
Roche Harold <rocheharold13@gmail.com>
2022-10-21 12:40:09 +0300

    update changes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -12,6 +12,7 @@ import { BsLinkedin } from "react-icons/bs";
 import { FaBed } from "react-icons/fa";
 import { GiBathtub } from "react-icons/gi";
 import { MdOutlineSquareFoot } from "react-icons/md";
+import { FiSearch } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
@@ -49,63 +50,23 @@ const Home = () => {
     },
   };
   return (
-    <section className="home con">
+    <section className="home ">
       <div className="home-header">
+        <div className="lay-easy"></div>
         <h2>Your Property is Our Priority</h2>
-        <p>
-          A small river named Duden flows by their place and supplies it with
-          the necessary regelialia.
-        </p>
-      </div>
-      <div className="search-bar">
-        <div className="picker">
-          <select className="picker-a">
-            <option value="Buy Property"> Buy Property</option>
-            <option value="Rent Property">Rent Property</option>
-            <option value="Sell Property"> Sell Property</option>
-          </select>
-        </div>
-        <div className="picker">
-          <select className="picker-a">
-            <option value="Apartments">Apartments</option>
-            <option value="Offices"> Offices</option>
-            <option value="TownHome"> TownHome</option>
-            <option value="Houses">Houses</option>
-            <option value="Studio">Studio</option>
-          </select>
-        </div>
-        <div className="picker">
-          <select className="picker-a">
-            <option value="New York">New York</option>
-            <option value="Nairobi">Nairobi</option>
-            <option value="Kisumu">Kisumu</option>
-            <option value="Tulum">Tulum</option>
-            <option value="Cualican">Cualican</option>
-            <option value="San Migel">San Migel</option>
-            <option value="San Diego">San Diego</option>
-            <option value="Los Angeles">Los Angeles</option>
-            <option value="Chicago">Chicago</option>
-            <option value="Hong Kong">Hong Kong</option>
-            <option value="Kyiv">Kyiv</option>
-            <option value="Moscow">Moscow</option>
-            <option value="Amsterdam">Amsterdam</option>
-            <option value="Cairo">Cairo</option>
-            <option value="Pretoria">Pretoria</option>
-            <option value="Male">Male</option>
-            <option value="Kampala">Kampala</option>
-            <option value="Dodoma">Dodoma</option>
-            <option value="Luanda">Luanda</option>
-            <option value="Marrackech">Marrakech</option>
-          </select>
-        </div>
-        <div className="picker-b">
-          <button className="picker-a ">
-            <a href="#button">
-              <h5>Search</h5>
-            </a>
-          </button>
+        <p>Turn your real-estate dreams into a reality </p>
+        <div className="header-put">
+          <input
+            className="input-fire"
+            type="text"
+            placeholder="Enter city, town or neighborhood"
+          />
+          <h3>
+            <FiSearch />
+          </h3>
         </div>
       </div>
+
       <div className="home-page container-fluid">
         <div className="featured text-center row">
           <h4>Discover Our Featured Listings</h4>
