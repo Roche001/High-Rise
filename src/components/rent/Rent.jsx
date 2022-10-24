da518c2babcfe09d7526b57873db66b340c0de80
Roche Harold <rocheharold13@gmail.com>
2022-10-24 09:36:55 +0300

    update changes

--- src/components/rent/Rent.jsx
+++ src/components/rent/Rent.jsx
@@ -1,10 +1,7 @@
-"react";
+import React from "react";
 import "./Rent.css";
-
 import { RiHome6Line } from "react-icons/ri";
 import { Link } from "react-router-dom";
-import { GiStreetLight } from "react-icons/gi";
-import { IoIosTrain } from "react-icons/io";
 import { BsDot } from "react-icons/bs";
 import { BsSearch } from "react-icons/bs";
 import {
@@ -18,15 +15,18 @@ const Rent = () => {
   const MapWithAMarker = withScriptjs(
     withGoogleMap((props) => (
       <GoogleMap
-        defaultZoom={15}
+        defaultZoom={10}
         defaultCenter={{ lat: -1.2306616792179152, lng: 36.84835920697511 }}
       >
         <Marker
           position={{ lat: -1.2306616792179152, lng: 36.84835920697511 }}
         />
+        <Marker position={{ lat: -1.250349, lng: 36.641401 }} />{" "}
+        <Marker position={{ lat: -1.32249, lng: 36.884941 }} />
       </GoogleMap>
     ))
   );
+
   return (
     <section id="rent">
       <div className="town-home">
@@ -42,12 +42,12 @@ const Rent = () => {
         </div>
       </div>
 
-      <div className=" container-fluid rent-townhomes">
+      <div className="rent-townhomes">
         <div className="maps-google">
           <MapWithAMarker
             googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBAXOQcxZzrxgrzdLhtRcmdBaiPEWzFkD8"
             loadingElement={<div style={{ height: `100%` }} />}
-            containerElement={<div style={{ height: `750px` }} />}
+            containerElement={<div style={{ height: `800px` }} />}
             mapElement={<div style={{ height: `100%` }} />}
           />
         </div>
@@ -76,18 +76,13 @@ const Rent = () => {
               </div>
               <div className="card-text">
                 <div className="card-head">
-                  <h4>9631 North Bridle East Bridgewater, MA 02333</h4>
+                  <h4>9631 North Bridle </h4>
                   <h5>$1400</h5>
                 </div>
                 <h6>
                   5Bedrooms <BsDot /> 5Bathrooms <BsDot /> 4,558 SQ FT <BsDot />
                   381.9 Acre(s)
                 </h6>
-                <div className="butt-on">
-                  <a href="Button">
-                    <Link to="/House1">See More</Link>
-                  </a>
-                </div>
               </div>
             </div>
             <div className="ranch-card nopadding ">
@@ -113,18 +108,13 @@ const Rent = () => {
               </div>
               <div className="card-text">
                 <div className="card-head">
-                  <h4>2800 Leverton Cove Road, MA 01069</h4>
+                  <h4>Leverton Cove Road, MA 01069</h4>
                   <h5>$2600</h5>
                 </div>
                 <h6>
                   5Bedrooms <BsDot /> 5Bathrooms <BsDot /> 4,558 SQ FT <BsDot />
                   381.9 Acre(s)
                 </h6>
-                <div className="butt-on">
-                  <a href="Button">
-                    <Link to="/House2">See More</Link>
-                  </a>
-                </div>
               </div>
             </div>
           </div>
@@ -152,18 +142,13 @@ const Rent = () => {
               </div>
               <div className="card-text">
                 <div className="card-head">
-                  <h4>4895 North Drive East Bridgewater, MA 05897</h4>
+                  <h4>North Drive East Bridgewater, MA 05897</h4>
                   <h5>$1250</h5>
                 </div>
                 <h6>
                   5Bedrooms <BsDot /> 5Bathrooms <BsDot /> 4,558 SQ FT <BsDot />
                   381.9 Acre(s)
                 </h6>
-                <div className="butt-on">
-                  <a href="Button">
-                    <Link to="/House3">See More</Link>
-                  </a>
-                </div>
               </div>
             </div>
             <div className="ranch-card nopadding ">
@@ -189,18 +174,13 @@ const Rent = () => {
               </div>
               <div className="card-text">
                 <div className="card-head">
-                  <h4>4895 North Drive East Bridgewater, MA 05897</h4>
+                  <h4>North Drive East Bridgewater, MA 05897</h4>
                   <h5>$1250</h5>
                 </div>
                 <h6>
                   5Bedrooms <BsDot /> 5Bathrooms <BsDot /> 4,558 SQ FT <BsDot />
                   381.9 Acre(s)
                 </h6>
-                <div className="butt-on">
-                  <a href="Button">
-                    <Link to="/House4">See More</Link>
-                  </a>
-                </div>
               </div>
             </div>
           </div>
@@ -229,18 +209,13 @@ const Rent = () => {
               </div>
               <div className="card-text">
                 <div className="card-head">
-                  <h4>8055 Leverton Cove Road, MA 01337</h4>
+                  <h4>Leverton Cove Road, MA 01337</h4>
                   <h5>$1550</h5>
                 </div>
                 <h6>
                   5Bedrooms <BsDot /> 5Bathrooms <BsDot /> 4,558 SQ FT <BsDot />
                   381.9 Acre(s)
                 </h6>
-                <div className="butt-on">
-                  <a href="Button">
-                    <Link to="/House5">See More</Link>
-                  </a>
-                </div>
               </div>
             </div>
             <div className="ranch-card nopadding ">
@@ -266,18 +241,13 @@ const Rent = () => {
               </div>
               <div className="card-text">
                 <div className="card-head">
-                  <h4>8055 Leverton Cove Road, MA 01337</h4>
+                  <h4>Leverton Cove Road, MA 01337</h4>
                   <h5>$1550</h5>
                 </div>
                 <h6>
                   5Bedrooms <BsDot /> 5Bathrooms <BsDot /> 4,558 SQ FT <BsDot />
                   381.9 Acre(s)
                 </h6>
-                <div className="butt-on">
-                  <a href="Button">
-                    <Link to="/House6">See More</Link>
-                  </a>
-                </div>
               </div>
             </div>
           </div>
@@ -305,18 +275,13 @@ const Rent = () => {
               </div>
               <div className="card-text">
                 <div className="card-head">
-                  <h4>8055 Leverton Cove Road, MA 01337</h4>
+                  <h4>Leverton Cove Road, MA 01337</h4>
                   <h5>$1550</h5>
                 </div>
                 <h6>
                   5Bedrooms <BsDot /> 5Bathrooms <BsDot /> 4,558 SQ FT <BsDot />
                   381.9 Acre(s)
                 </h6>
-                <div className="butt-on">
-                  <a href="Button">
-                    <Link to="/House7">See More</Link>
-                  </a>
-                </div>
               </div>
             </div>
             <div className="ranch-card nopadding ">
@@ -342,18 +307,13 @@ const Rent = () => {
               </div>
               <div className="card-text">
                 <div className="card-head">
-                  <h4>9631 North Bridle East Bridgewater, MA 02333</h4>
+                  <h4>North Bridle East Bridgewater, MA 02333</h4>
                   <h5>$2400</h5>
                 </div>
                 <h6>
                   5Bedrooms <BsDot /> 5Bathrooms <BsDot /> 4,558 SQ FT <BsDot />
                   381.9 Acre(s)
                 </h6>
-                <div className="butt-on">
-                  <a href="Button">
-                    <Link to="/House8">See More</Link>
-                  </a>
-                </div>
               </div>
             </div>
           </div>
@@ -377,13 +337,10 @@ const Rent = () => {
                   </span>
                 </div>
                 <div className="card-text">
-                  <h4>5884 Gudme</h4>
-                  <h4>5MHV+6Q7 Gudme, Denmark</h4>
+                  <h4>Gudme, Denmark</h4>
                   <h5>$8000</h5>
                   <h6>
                     5Bedrooms <BsDot /> 5Bathrooms <BsDot /> 4,558 SQ FT{" "}
-                    <BsDot />
-                    381.9 Acre(s)
                   </h6>
                   <small>
                     Marketed By Vargas Camino's International Realty
@@ -406,13 +363,10 @@ const Rent = () => {
                   </span>
                 </div>
                 <div className="card-text">
-                  <h4>1839 Honey Spring Place</h4>
-                  <h4>Lexington,KY,01511 United States</h4>
+                  <h4>Lexington, United States</h4>
                   <h5>$25,000</h5>
                   <h6>
-                    9Bedrooms <BsDot /> 9Bathrooms <BsDot /> 10,558 SQ FT{" "}
-                    <BsDot />
-                    381.9 Acre(s)
+                    9Bedrooms <BsDot /> 9Bathrooms <BsDot /> 1558 SQ FT{" "}
                   </h6>
                   <small>
                     Marketed By Vargas Camino's International Realty
@@ -437,13 +391,10 @@ const Rent = () => {
                   </span>
                 </div>
                 <div className="card-text">
-                  <h4>5225 Greenwhich Pike</h4>
-                  <h4>Merna,NE,40511 United States</h4>
+                  <h4>Merna,United States</h4>
                   <h5>$32,000</h5>
                   <h6>
-                    8Bedrooms <BsDot /> 8Bathrooms <BsDot /> 12,558 SQ FT{" "}
-                    <BsDot />
-                    381.9 Acre(s)
+                    8Bedrooms <BsDot /> 8Bathrooms <BsDot /> 4558 SQ FT{" "}
                   </h6>
                   <small>
                     Marketed By Vargas Camino's International Realty
@@ -466,13 +417,10 @@ const Rent = () => {
                   </span>
                 </div>
                 <div className="card-text">
-                  <h4>3022 Valley Road</h4>
                   <h4>Bodhei,MBS,40511 Kenya</h4>
                   <h5>$2000</h5>
                   <h6>
                     5Bedrooms <BsDot /> 5Bathrooms <BsDot /> 4,558 SQ FT{" "}
-                    <BsDot />
-                    381.9 Acre(s)
                   </h6>
                   <small>
                     Marketed By Vargas Camino's International Realty
@@ -498,13 +446,10 @@ const Rent = () => {
                   </span>
                 </div>
                 <div className="card-text">
-                  <h4>3000 Kajulu Hills</h4>
-                  <h4>Kisumu,ksm,2330 Kenya</h4>
+                  <h4>Kisumu,ksm, Kenya</h4>
                   <h5>$32,000</h5>
                   <h6>
                     5Bedrooms <BsDot /> 5Bathrooms <BsDot /> 4,558 SQ FT{" "}
-                    <BsDot />
-                    381.9 Acre(s)
                   </h6>
                   <small>
                     Marketed By Vargas Camino's International Realty
@@ -527,13 +472,10 @@ const Rent = () => {
                   </span>
                 </div>
                 <div className="card-text">
-                  <h4>39000 Eswatzini Road</h4>
                   <h4>Beaufort,BW,4201 South Africa</h4>
                   <h5>$29,000</h5>
                   <h6>
                     5Bedrooms <BsDot /> 5Bathrooms <BsDot /> 4,558 SQ FT{" "}
-                    <BsDot />
-                    381.9 Acre(s)
                   </h6>
                   <small>
                     Marketed By Vargas Camino's International Realty
@@ -541,7 +483,7 @@ const Rent = () => {
                 </div>
               </div>{" "}
             </div>
-            <div className="ranch-container">
+            <div className="ranch-container row">
               <div className="ranch-card nopadding ">
                 <div className="card-img">
                   <Link to="/House15">
@@ -558,13 +500,10 @@ const Rent = () => {
                   </span>
                 </div>
                 <div className="card-text">
-                  <h4>9088 Kilamba Road</h4>
                   <h4>Yembe,YM,1100 Angola</h4>
                   <h5>$18,000</h5>
                   <h6>
                     5Bedrooms <BsDot /> 5Bathrooms <BsDot /> 4,558 SQ FT{" "}
-                    <BsDot />
-                    381.9 Acre(s)
                   </h6>
                   <small>
                     Marketed By Vargas Camino's International Realty
@@ -587,13 +526,10 @@ const Rent = () => {
                   </span>
                 </div>
                 <div className="card-text">
-                  <h4>5088 Al Akrad</h4>
                   <h4>Al Akrad,KY,5110 United Egypt</h4>
                   <h5>$27,000</h5>
                   <h6>
                     9Bedrooms <BsDot /> 9Bathrooms <BsDot /> 4,558 SQ FT{" "}
-                    <BsDot />
-                    381.9 Acre(s)
                   </h6>
                   <small>
                     Marketed By Vargas Camino's International Realty
@@ -627,22 +563,6 @@ const Rent = () => {
                     Move In <BsDot />
                     Fixed Cost
                   </h6>
-                  <small>
-                    Administrative support, Break lounge, Meeting
-                    rooms,Conference rooms
-                  </small>
-                  <h4>
-                    <GiStreetLight />{" "}
-                    <span>
-                      Johnstone st._<small>0.3mi</small>
-                    </span>
-                  </h4>
-                  <h4>
-                    <IoIosTrain />{" "}
-                    <span>
-                      Train Station_<small>0.9mi</small>
-                    </span>
-                  </h4>
                 </div>
               </div>
               <div className="ranch-card nopadding">
@@ -668,22 +588,6 @@ const Rent = () => {
                     Move In <BsDot />
                     Fixed Cost
                   </h6>
-                  <small>
-                    Administrative support, Break lounge, Meeting
-                    rooms,Conference rooms
-                  </small>
-                  <h4>
-                    <GiStreetLight />{" "}
-                    <span>
-                      Gobelas st._<small>0.9mi</small>
-                    </span>
-                  </h4>
-                  <h4>
-                    <IoIosTrain />{" "}
-                    <span>
-                      Train Station_<small>0.3mi</small>
-                    </span>
-                  </h4>
                 </div>
               </div>{" "}
             </div>
@@ -711,22 +615,6 @@ const Rent = () => {
                     Move In <BsDot />
                     Fixed Cost
                   </h6>
-                  <small>
-                    Administrative support, Break lounge, Meeting
-                    rooms,Conference rooms
-                  </small>
-                  <h4>
-                    <GiStreetLight />{" "}
-                    <span>
-                      Ribera st._<small>0.1mi</small>
-                    </span>
-                  </h4>
-                  <h4>
-                    <IoIosTrain />{" "}
-                    <span>
-                      Train Station_<small>0.15mi</small>
-                    </span>
-                  </h4>
                 </div>
               </div>
               <div className="ranch-card nopadding ">
@@ -752,22 +640,6 @@ const Rent = () => {
                     Move In <BsDot />
                     Fixed Cost
                   </h6>
-                  <small>
-                    Administrative support, Break lounge, Meeting
-                    rooms,Conference rooms
-                  </small>
-                  <h4>
-                    <GiStreetLight />{" "}
-                    <span>
-                      Vicálvaro._<small>0.8mi</small>
-                    </span>
-                  </h4>
-                  <h4>
-                    <IoIosTrain />{" "}
-                    <span>
-                      Train Station_<small>0.35mi</small>
-                    </span>
-                  </h4>
                 </div>
               </div>
             </div>
@@ -796,22 +668,6 @@ const Rent = () => {
                     Move In <BsDot />
                     Fixed Cost
                   </h6>
-                  <small>
-                    Administrative support, Break lounge, Meeting
-                    rooms,Conference rooms
-                  </small>
-                  <h4>
-                    <GiStreetLight />{" "}
-                    <span>
-                      Vicálvaro st._<small>0.8mi</small>
-                    </span>
-                  </h4>
-                  <h4>
-                    <IoIosTrain />{" "}
-                    <span>
-                      Train Station_<small>0.35mi</small>
-                    </span>
-                  </h4>
                 </div>
               </div>
               <div className="ranch-card nopadding ">
@@ -837,22 +693,6 @@ const Rent = () => {
                     Move In <BsDot />
                     Fixed Cost
                   </h6>
-                  <small>
-                    Administrative support, Break lounge, Meeting
-                    rooms,Conference rooms
-                  </small>
-                  <h4>
-                    <GiStreetLight />{" "}
-                    <span>
-                      Madrid st._<small>0.8mi</small>
-                    </span>
-                  </h4>
-                  <h4>
-                    <IoIosTrain />{" "}
-                    <span>
-                      Train Station_<small>0.35mi</small>
-                    </span>
-                  </h4>
                 </div>
               </div>{" "}
             </div>
@@ -880,22 +720,6 @@ const Rent = () => {
                     Move In <BsDot />
                     Fixed Cost
                   </h6>
-                  <small>
-                    Administrative support, Break lounge, Meeting
-                    rooms,Conference rooms
-                  </small>
-                  <h4>
-                    <GiStreetLight />{" "}
-                    <span>
-                      Puerto de st._<small>1mi</small>
-                    </span>
-                  </h4>
-                  <h4>
-                    <IoIosTrain />{" "}
-                    <span>
-                      Train Station_<small>0.9mi</small>
-                    </span>
-                  </h4>
                 </div>
               </div>
               <div className="ranch-card nopadding ">
@@ -921,22 +745,6 @@ const Rent = () => {
                     Move In <BsDot />
                     Fixed Cost
                   </h6>
-                  <small>
-                    Administrative support, Break lounge, Meeting
-                    rooms,Conference rooms
-                  </small>
-                  <h4>
-                    <GiStreetLight />{" "}
-                    <span>
-                      Jerónimos st._<small>1mi</small>
-                    </span>
-                  </h4>
-                  <h4>
-                    <IoIosTrain />{" "}
-                    <span>
-                      Train Station_<small>0.9mi</small>
-                    </span>
-                  </h4>
                 </div>
               </div>
             </div>
