982d7a1866ee3d17c6e464e51a722df64d3d70cc
Roche Harold <rocheharold13@gmail.com>
2022-11-01 20:59:20 +0300

    update changes

--- src/components/sell/Sell.jsx
+++ src/components/sell/Sell.jsx
@@ -1,60 +1,67 @@
 import React from "react";
 import "./Sell.css";
+import { Link } from "react-router-dom";
+
 const Sell = () => {
   return (
-    <div className="sell-header container-fluid">
-      <h3>Why Sell Your House with High-Rise</h3>
-      <div className="sell-container row">
-        <div className="sell-card nopadding ">
-          <div className="sell-card-img">
-            <img
-              className="d-block w-100"
-              src="./assets/sell1.jpeg"
-              alt="welcome"
-            />
-          </div>
-          <div className="sell-card-text">
-            <h5>Personalised Real-estate Service</h5>
+    <div className="sell-block container-fluid nopadding">
+      <div className="section-header">
+        <h3>Explore Available Selling Options</h3>
+        <p>
+          Wherever you are in the selling journey, our hassle-free tools and
+          guides will help you make the right move.
+        </p>
+      </div>
+      <div className="section-bg">
+        <h3>Enter Your Address to Unlock Your Owner Dashboard</h3>
+        <div className="section-listing">
+          <div className="section-one">
+            <div className="sell-image">
+              <img src="./assets/sell-one.jpeg" alt="sell" />
+            </div>
+            <h5>See Your StreetEasy Valuation</h5>
             <p>
-              Our agents have the expertise to earn you top dollar and ensure
-              that the entire sale process—from valuation to closure-goes
-              without a hitch.
+              Get an instant, hassle-free estimate of your home’s value, powered
+              by the most expansive real estate database in NYC.
+            </p>
+          </div>
+          <div className="section-one">
+            <div className="sell-image">
+              <img src="./assets/sell-two.jpeg" alt="sell" />
+            </div>
+            <h5>Get Matched With a Trusted Agent</h5>
+            <p>
+              Ready to sell? Just have questions? We can recommend listing
+              agents who’ve closed on homes just like yours.
             </p>
           </div>
         </div>
-        <div className="sell-card nopadding ">
-          <div className="sell-card-img">
-            <img
-              className="d-block w-100"
-              src="./assets/sell2.jpeg"
-              alt="welcome"
+        <div className="dragon-dance">
+          <div className="header-put-one">
+            <input
+              className="input-fire"
+              type="text"
+              placeholder="445 STREET NAME, NAIROBI, NRB 234"
             />
           </div>
-          <div className="sell-card-text">
-            <h5>Access to a wide range of customers</h5>
-            <p>
-              The firm's listings receive 50% more views from potential buyers
-              thanks to targeted marketing across our website, mobile
-              application and emails.
-            </p>
-          </div>
-        </div>
-        <div className="sell-card nopadding ">
-          <div className="sell-card-img">
-            <img
-              className="d-block w-100"
-              src="./assets/sell3.jpeg"
-              alt="welcome"
+          <div className="header-put-two">
+            <input
+              className="input-fire"
+              type="text"
+              placeholder="UNIT NUMBER"
             />
           </div>
-          <div className="sell-card-text">
-            <h5>Pay 2% fee when you buy or sell</h5>
-            <p>
-              Our clients sell for more money on average, and when you sell and
-              buy with us, you pay a 2% listing fee.
-            </p>
-          </div>
-        </div>
+          <div className="push-on">
+            <h6>Get Started</h6>
+          </div>{" "}
+        </div>{" "}
+        <p>
+          Are you a returning home owner?{" "}
+          <span>
+            <Link to="./Profile1"> Log in</Link>
+          </span>{" "}
+          to view your Owner Dashboard.
+        </p>
       </div>
     </div>
   );
