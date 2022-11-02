218f46a91519a05daf6ae7b4a4268979ae05a437
Roche Harold <rocheharold13@gmail.com>
2022-11-02 23:19:49 +0300

    update changes

--- src/components/sell/Sell.jsx
+++ src/components/sell/Sell.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import "./Sell.css";
-import { Link } from "react-router-dom";
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "95%",
+  height: "50%",
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const Sell = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
     <div className="sell-block container-fluid nopadding">
       <div className="section-header">
@@ -55,10 +76,54 @@ const Sell = () => {
             <h6>Get Started</h6>
           </div>{" "}
         </div>{" "}
-        <p>
-          Are you a returning home owner? <span>Log in</span> to view your Owner
-          Dashboard.
-        </p>
+        <div>
+          <p>
+            Are you a returning home owner?
+            <Button onClick={handleOpen}>log in</Button> to view your Owner
+            Dashboard.
+          </p>{" "}
+          <div className="modal-small">
+            <Modal
+              aria-labelledby="transition-modal-title"
+              aria-describedby="transition-modal-description"
+              open={open}
+              onClose={handleClose}
+              closeAfterTransition
+              BackdropComponent={Backdrop}
+              BackdropProps={{
+                timeout: 500,
+              }}
+            >
+              <Fade in={open}>
+                <Box sx={style}>
+                  <Typography
+                    id="transition-modal-title"
+                    variant="h6"
+                    component="h2"
+                  >
+                    <div className="log-out">
+                      <h4>Login</h4>
+                    </div>
+                  </Typography>
+                  <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+                    <div className="log-in">
+                      <input
+                        className="input-fire"
+                        type="text"
+                        placeholder=" Email"
+                      />
+                    </div>
+                    <div className="winterfell">
+                      <a href="#button">
+                        <h6>SUBMIT</h6>
+                      </a>
+                    </div>
+                  </Typography>
+                </Box>
+              </Fade>
+            </Modal>
+          </div>
+        </div>
       </div>
     </div>
   );
