d3e9609d4ab0f1a922c83fc6e5322ff7c6216a45
Roche Harold <rocheharold13@gmail.com>
2022-11-06 15:47:29 +0300

    update changes

--- src/components/profile1/Profile1.jsx
+++ src/components/profile1/Profile1.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import "./Profile1.css";
 import { FiCircle } from "react-icons/fi";
 const Profile1 = () => {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <div className="prof-box">
+        <h4>Karen Masterson</h4>
+        <div className="prof-card-one"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile1;
