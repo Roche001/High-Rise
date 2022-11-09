c44664949dc625391386cbf9e0c4b96b0ed6037b
Roche Harold <rocheharold13@gmail.com>
2022-11-09 14:42:07 +0300

    update changes

--- src/components/sell/Sell.jsx
+++ src/components/sell/Sell.jsx
@@ -1,57 +1,38 @@
 import React from "react";
 import "./Sell.css";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
-import Button from "@mui/material/Button";
 
 const Sell = () => {
-  const [select, setAge] = React.useState("");
-  const [open, setOpen] = React.useState(false);
-
-  const handleChange = (event) => {
-    setAge(event.target.value);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
   return (
     <div className="submit-container container">
       <div className="submit-card-bg">
-        <div className="submit-options">
-          <div>
-            <Button sx={{ display: "block", mt: 2 }} onClick={handleOpen}>
-              Open the select
-            </Button>
-            <FormControl sx={{ m: 1, minWidth: "60%" }}>
-              <InputLabel id="demo-controlled-open-select-label">
-                Select Option
-              </InputLabel>
-              <Select
-                labelId="demo-controlled-open-select-label"
-                id="demo-controlled-open-select"
-                open={open}
-                onClose={handleClose}
-                onOpen={handleOpen}
-                value={select}
-                label="select"
-                onChange={handleChange}
-              >
-                <MenuItem value="">
-                  <em>None</em>
-                </MenuItem>
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
-              </Select>
-            </FormControl>
+        <h4>Basic Information </h4>
+        <div className="submit-small">
+          <div className="small-one">
+            <label htmlFor="Title">Title</label>
+            <input
+              className="input-sup"
+              type="text"
+              placeholder="Enter House Title/Name"
+            />
           </div>
+          <div className="small-one">
+            <label htmlFor="Title">Price</label>
+            <input
+              className="input-sub"
+              type="text"
+              placeholder="Enter Price"
+            />
+          </div>
+        </div>
+        <div className="text-area">
+          <label htmlFor="Title">Description</label>
+          <textarea
+            name="textarea"
+            id="textarea"
+            cols="30"
+            rows="5"
+            placeholder="House Description"
+          ></textarea>
         </div>
       </div>
     </div>
