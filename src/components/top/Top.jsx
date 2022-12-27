0a9e4c7b06983c09df198291048d80c8c6eb404c
Roche Harold <rocheharold13@gmail.com>
2022-12-27 16:33:49 +0300

    update changes

--- src/components/top/Top.jsx
+++ src/components/top/Top.jsx
@@ -1,53 +1,8 @@
 import React from "react";
 import "./Top.css";
-import { Link } from "react-router-dom";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
 
 const Top = () => {
-  return (
-    <Navbar collapseOnSelect expand="lg" bg="" variant="dark" className="nav">
-      <Container className="navbar">
-        <Navbar.Brand>
-          <Link to="/">
-            <div className="logo">
-              <img src="./assets/home.png" alt="Company-logo" loading="lazy" />
-            </div>
-          </Link>
-        </Navbar.Brand>
-        <Navbar.Toggle
-          aria-controls="responsive-navbar-nav"
-          className="toggle"
-        />
-
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link>
-              <Link to="/" className="link">
-                <li>Home</li>
-              </Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/Rent" className="link">
-                <li>Rent</li>
-              </Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/Buy" className="link">
-                <li>Buy</li>
-              </Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/Submit" className="link">
-                <li>Submit</li>
-              </Link>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+  return <div className="top-container"></div>;
 };
 
 export default Top;
