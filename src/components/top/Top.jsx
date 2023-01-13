8e9f1b5e884b006f53324703750ee193573c7e24
Roche001 <rocheharold13@gmail.com>
2023-01-13 12:09:33 +0300

    update changes

--- src/components/top/Top.jsx
+++ src/components/top/Top.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import "./Top.css";
+import { topper } from "../../nav";
+import { Link } from "react-router-dom";
 
 const Top = () => {
-  return <div className="navbar"></div>;
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <h1>Harmohomes</h1>
+        <h5>Real Estate Agency</h5>
+      </div>
+      <div className="menu-items">
+        {topper.map((topper, index) => {
+          return (
+            <div className="menu-one" key={index}>
+              <Link to={topper.navlink}>{topper.title}</Link>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Top;
--- src/nav.js
+++ src/nav.js
@@ -1,1 +1,7 @@
-export const top = [{ navlink: "/", title: "Home" }];
+export const topper = [
+  { navlink: "/", title: "Home" },
+  { navlink: "/Rent", title: "Rent" },
+  { navlink: "/Buy", title: "Buy" },
+  { navlink: "/Submit", title: "Submit" },
+  { navlink: "/Blog", title: "Blog" },
+];
