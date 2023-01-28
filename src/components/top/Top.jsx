ba80fe85efecfa21fb42e82d0bf1d8a6e4d6b04a
Roche001 <rocheharold13@gmail.com>
2023-01-28 10:12:15 +0300

    update changes

--- src/components/top/Top.jsx
+++ src/components/top/Top.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./Top.css";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { AiOutlineClose } from "react-icons/ai";
+// import { AiOutlineClose } from "react-icons/ai";
 import { topper } from "../../nav";
 import { Link } from "react-router-dom";
 
@@ -30,44 +30,42 @@ const Top = () => {
   }, []);
 
   return (
-    <div
-      className="top-bar"
-      style={{ backgroundColor: navbarColor }}
-      onClick={handleClick}
-    >
+    <div className="top-bar" style={{ backgroundColor: navbarColor }}>
       <div className="logo">
         <Link to="/">
           <div className="logo-one">
             <img src="./assets/logo.png" alt="logo" loading="lazy" />
           </div>
         </Link>
-      </div>
-      <div className="top-icon" onClick={toggleMenu}>
-        <h4>
-          <GiHamburgerMenu />
-        </h4>
-      </div>
+      </div>{" "}
+      <div className="handle" onClick={handleClick}>
+        <div className="top-icon" onClick={toggleMenu}>
+          <h4>
+            <GiHamburgerMenu />
+          </h4>
+        </div>
 
-      <div className={menuOpen ? "menu-items-one" : "menu-items"}>
-        <div className="top-close" onClick={handleClick}>
+        <div className={menuOpen ? "menu-items-one" : "menu-items"}>
+          {/* <div className="top-close" onClick={handleClick}>
           <h4>
             <AiOutlineClose />
           </h4>
-        </div>
-        <div className="menu-open">
-          {topper.map((topper, index) => {
-            return (
-              <div key={index}>
-                <Link
-                  to={topper.navlink}
-                  className="menu-one"
-                  onClick={handleClick}
-                >
-                  {topper.title}
-                </Link>
-              </div>
-            );
-          })}
+        </div> */}
+          <div className="menu-open">
+            {topper.map((topper, index) => {
+              return (
+                <div key={index}>
+                  <Link
+                    to={topper.navlink}
+                    className="menu-one"
+                    onClick={handleClick}
+                  >
+                    {topper.title}
+                  </Link>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
