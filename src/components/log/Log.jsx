d67c6adb25a5544e8715bf30bfecab901c9341cc
Roche001 <rocheharold13@gmail.com>
2023-02-27 13:50:17 +0300

    update changes

--- src/components/log/Log.jsx
+++ src/components/log/Log.jsx
@@ -8,25 +8,6 @@ import { Si1Password } from "react-icons/si";
 const Log = () => {
   return (
     <div className="log-bg">
-      <div className="log-container">
-        <Link to="/Log/In" className="login-page">
-          <h5>
-            <FaUserCheck />
-            Login
-          </h5>
-        </Link>
-        <Link to="/Log/Register" className="login-page">
-          <h5>
-            <GiArchiveRegister />
-            Register
-          </h5>
-        </Link>
-        <Link to="/Log/Pass" className="login-page">
-          <h5>
-            <Si1Password /> Forgot Password?
-          </h5>
-        </Link>
-      </div>
       <Outlet />
     </div>
   );
