fc14d4523377c0bdeb17e6d259d7e4c34e07e8b1
Roche001 <rocheharold13@gmail.com>
2023-03-04 07:51:20 +0300

    update changes

--- src/components/pass/Pass.jsx
+++ src/components/pass/Pass.jsx
@@ -2,41 +2,13 @@ import React, { useState } from "react";
 import "./Pass.css";
 
 const Pass = () => {
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetch("/api/password-reset", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email }),
-      });
-      const data = await response.json();
-      setMessage(data.message);
-    } catch (error) {
-      setMessage(error.message);
-    }
-  };
   return (
     <div className="pass-big">
       <div className="pass-container">
         <h4>Reset your password</h4>
-        <form onSubmit={handleSubmit}>
-          <input
-            className="in-small"
-            placeholder="Email"
-            required
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <button className="reset">Send a passworld reset link</button>
-        </form>
-        {message && <p>{message}</p>}
+
+        <input className="in-small" placeholder="Email" required type="email" />
+        <button className="reset">Send a passworld reset link</button>
       </div>
     </div>
   );
